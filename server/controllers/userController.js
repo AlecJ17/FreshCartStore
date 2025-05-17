0af6f1aacf2c16cfba6b38ecd918bddeaa35eda9
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2025-05-16 21:23:45 -0400

    Add login user route and logics to ensure the user's login are processing smoothly

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -19,13 +19,58 @@ export const register = async (req, res) => {
       email,
       password: hashedPassword,
     });
-    const token = jwt.sign({ id: user._id }, process.send.JWT_SECRET, {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
     res.cookie("token", token, {
       httpOnly: true, // Prevent JavaScript access to the cookie
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days cookie expiration
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Login User : /api/user/login
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "Email and Password Required",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days cookie expiration
     });
 
