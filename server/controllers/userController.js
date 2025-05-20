419f450c4a5be2fba0c927edf02fe4516f65ed27
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2025-05-20 01:32:30 -0400

    Fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,8 +24,8 @@ export const register = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true, // Prevent JavaScript access to the cookie
-      secure: true,
-      sameSite: none, // Use 'none' for cross-site cookies
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days cookie expiration
     });
 
@@ -69,8 +69,8 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: none,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days cookie expiration
     });
 
